ad472d2568fcbef198468ca9c1cfed53163c7aa0
Szczepan <s.szablej1@gmail.com>
2020-07-23 12:48:06 +0200

    :art: Add GlobalStyle

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap');
+  *,*::before,*::after{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html{
+    font-size: 62,5%;
+  }
+
+  body{
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1rem;
+  }
+`;
+
+export default GlobalStyle;
