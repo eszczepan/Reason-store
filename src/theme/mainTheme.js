21f01b1b9a2fee567d7ae5ad298f2a81cf46d3f1
Szczepan <s.szablej1@gmail.com>
2020-07-23 16:47:38 +0200

    :art: Add MainTemplate & mainTheme

---
+++ src/theme/mainTheme.js
@@ -0,0 +1,21 @@
+export const theme = {
+  primary: '#F2BB13',
+  secondary: '#F2A30F',
+  blue: '#04BFBF',
+  red: '#D93D1A',
+  black: '#0a0903',
+  grey100: 'hsl(0, 0%, 96%)',
+  grey200: 'hsl(0, 0%, 90%)',
+  grey300: 'hsl(0, 0%, 70%)',
+  light: 300,
+  normal: 400,
+  bold: 700,
+  fontSize: {
+    xxs: '1rem',
+    xs: '1.2rem',
+    s: '1.6rem',
+    m: '2.1rem',
+    l: '2.4rem',
+    xl: '4rem',
+  },
+};
