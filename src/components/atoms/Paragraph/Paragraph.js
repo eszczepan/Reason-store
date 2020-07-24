2ecd002e89cf8e6852ef25c342b96a4a69c6ace5
Szczepan <s.szablej1@gmail.com>
2020-07-24 17:18:30 +0200

    :sparkles: Add info props

--- src/components/atoms/Paragraph/Paragraph.js
+++ src/components/atoms/Paragraph/Paragraph.js
@@ -8,6 +8,18 @@ const Paragraph = styled.p`
       font-size: ${({ theme }) => theme.fontSize.s};
       font-weight: ${({ theme }) => theme.bold};
     `}
+  ${({ info }) =>
+    info &&
+    css`
+      font-size: ${({ theme }) => theme.fontSize.xl};
+      font-weight: ${({ theme }) => theme.bold};
+      @media (max-width: 700px) {
+        font-size: ${({ theme }) => theme.fontSize.m};
+      }
+      @media (max-width: 380px) {
+        font-size: ${({ theme }) => theme.fontSize.s};
+      }
+    `}
 `;
 
 export default Paragraph;
