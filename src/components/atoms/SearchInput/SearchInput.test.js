bacb7a97b0137486f9643a4dac14faf4e48eaf24
Szczepan <s.szablej1@gmail.com>
2020-07-26 16:04:22 +0200

    :rotating_light: Add SearchInput test

---
+++ src/components/atoms/SearchInput/SearchInput.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import SearchInput from './SearchInput';
+
+describe('SearchInput component', () => {
+  test('Renders input element', () => {
+    const placeholderText = 'search';
+    const { getByPlaceholderText } = render(
+      <SearchInput placeholder={placeholderText} />,
+    );
+
+    expect(getByPlaceholderText(placeholderText)).toBeInTheDocument();
+  });
+});
